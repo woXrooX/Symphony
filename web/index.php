6f833a3b38b803b02ec50ed0802e00c90afe00e1
woXrooX <woxroox@gmail.com>
2021-04-13 15:13:28 +0100

    1.1.1

--- web/index.php
+++ web/index.php
@@ -1,5 +1,19 @@
 <?php
   include_once 'php/HTML.php';
-  $main = "Main";
-  HTML::run("Home", $main);
+
+  ////////// home
+  if($_SERVER['REQUEST_URI'] == "/" || $_SERVER['REQUEST_URI'] == "/home"){
+    $main = "Home";
+    HTML::run("Home", $main);
+
+  ////////// privacyPolicy
+  }elseif($_SERVER['REQUEST_URI'] == "privacyPolicy"){
+    $main = "Home";
+    HTML::run("Home", $main);
+
+  ////////// redirect home
+  }else{
+    header("location: https://www.site.com/home");
+    exit();
+  }
 ?>
