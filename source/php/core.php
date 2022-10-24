99a17b5f508e88eedd99e23998299390765facbf
Yahya <woXrooX@gmail.com>
2022-10-24 18:32:22 +0200

    Init Conf class

--- source/php/core.php
+++ source/php/core.php
@@ -1,12 +1,14 @@
 <?php
+include_once 'configurations.php';
 include_once 'router.php';
 include_once 'HTML.php';
 
 class Core{
   public static function start(){
+    Configurations::init();
     Router::start();
     HTML::build();
-    
+
   }
 
 }
