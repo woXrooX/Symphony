0502524df2c1f48666bb3cfc1db61b9467b7b9d4
Yahya <woXrooX@gmail.com>
2022-10-25 15:33:20 +0200

    Initing Database

--- source/php/core.php
+++ source/php/core.php
@@ -1,11 +1,20 @@
 <?php
 include_once 'configurations.php';
+include_once 'database.php';
 include_once 'router.php';
 include_once 'HTML.php';
 
 class Core{
   public static function start(){
     Configurations::init();
+
+    Database::connect(
+      Conf::database()["host"],
+      Conf::database()["user"],
+      Conf::database()["password"],
+      Conf::database()["name"]
+    );
+
     Router::start();
     HTML::build();
 
