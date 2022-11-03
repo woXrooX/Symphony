4207963b1848a13227267bd1fad943e165b9150d
Yahya <woXrooX@gmail.com>
2022-11-03 13:55:08 +0100

    Namespace: Router Done, onGET and onPOST functions to create web pages

--- source/php/router.php
+++ source/php/router.php
@@ -1,13 +1,29 @@
 <?php
-class Router{
+namespace Symphony;
+
+final class Router{
   public static function start(){
     self::detectURL();
   }
 
   private static function detectURL(){
+    // Output Buffering ON
+    ob_start();
+
     // ON URL == / Go Home
     if($_SERVER['REQUEST_URI'] == "/"){
       require_once $_SERVER['DOCUMENT_ROOT'].'/pages/home.php';
+
+      // Output Buffering OFF + Erase Buffered Data
+      ob_end_clean();
+
+
+      // On Request Method == GET
+      if(function_exists("onGET") && $_SERVER["REQUEST_METHOD"] == "GET") HTML::setMain(onGET());
+
+      // On Request Method == POST
+      if(function_exists("onPOST") && $_SERVER["REQUEST_METHOD"] == "POST") require onPOST();
+
       return;
 
     }
@@ -15,6 +31,16 @@ class Router{
     // Check If File Exists For Specific URL
     if(file_exists($_SERVER['DOCUMENT_ROOT'].'/pages'.$_SERVER['REQUEST_URI'].'.php')){
       require_once $_SERVER['DOCUMENT_ROOT'].'/pages'.$_SERVER['REQUEST_URI'].'.php';
+
+      // Output Buffering OFF + Erase Buffered Data
+      ob_end_clean();
+
+      // On Request Method == GET
+      if(function_exists("onGET") && $_SERVER["REQUEST_METHOD"] == "GET") HTML::setMain(onGET());
+
+      // On Request Method == POST
+      if(function_exists("onPOST") && $_SERVER["REQUEST_METHOD"] == "POST") require onPOST();
+
       return;
 
     }
