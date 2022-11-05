106589adfc14343fb8eaaab162ae4baace9ab15e
Yahya <woXrooX@gmail.com>
2022-11-05 11:14:40 +0100

    Fixed preventing user defined pages from echoing - redundancies/repetitions + more clean code

--- source/php/router.php
+++ source/php/router.php
@@ -11,42 +11,22 @@ final class Router{
     ob_start();
 
     // ON URL == / Go Home
-    if($_SERVER['REQUEST_URI'] == "/"){
-      require_once $_SERVER['DOCUMENT_ROOT'].'/pages/home.php';
-
-      // Output Buffering OFF + Erase Buffered Data
-      ob_end_clean();
-
-
-      // On Request Method == GET
-      if(function_exists("onGET") && $_SERVER["REQUEST_METHOD"] == "GET") HTML::setMain(onGET());
-
-      // On Request Method == POST
-      if(function_exists("onPOST") && $_SERVER["REQUEST_METHOD"] == "POST") return onPOST();
-
-      return;
-
-    }
+    if($_SERVER['REQUEST_URI'] == "/") require_once $_SERVER['DOCUMENT_ROOT'].'/pages/home.php';
 
     // Check If File Exists For Specific URL
-    if(file_exists($_SERVER['DOCUMENT_ROOT'].'/pages'.$_SERVER['REQUEST_URI'].'.php')){
-      require_once $_SERVER['DOCUMENT_ROOT'].'/pages'.$_SERVER['REQUEST_URI'].'.php';
-
-      // Output Buffering OFF + Erase Buffered Data
-      ob_end_clean();
-
-      // On Request Method == GET
-      if(function_exists("onGET") && $_SERVER["REQUEST_METHOD"] == "GET") HTML::setMain(onGET());
-
-      // On Request Method == POST
-      if(function_exists("onPOST") && $_SERVER["REQUEST_METHOD"] == "POST") return onPOST();
-
-      return;
-
-    }
+    elseif(file_exists($_SERVER['DOCUMENT_ROOT'].'/pages'.$_SERVER['REQUEST_URI'].'.php')) require_once $_SERVER['DOCUMENT_ROOT'].'/pages'.$_SERVER['REQUEST_URI'].'.php';
 
     // 404 | Back To Home
-    header("location: http://".$_SERVER['HTTP_HOST']."/home");
+    else header("location: http://".$_SERVER['HTTP_HOST']."/home");
+
+    // On Request Method == GET
+    if(function_exists("onGET") && $_SERVER["REQUEST_METHOD"] == "GET") HTML::setMain(onGET());
+
+    // On Request Method == POST
+    if(function_exists("onPOST") && $_SERVER["REQUEST_METHOD"] == "POST") return onPOST();
+
+    // Output Buffering OFF + Erase Buffered Data
+    ob_end_clean();
 
   }
 
