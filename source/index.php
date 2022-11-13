2871802ad06a55f7e91ba0167b08959d9205774a
woXrooX <woXrooX@gmail.com>
2022-11-13 16:00:17 +0100

    re-order

--- source/index.php
+++ source/index.php
@@ -1,12 +1,12 @@
 <?php
 require_once 'php/core.php';
 
-// Setting JSON As Default Configurations File
-// Symphony\Configurations::useJSON();
-
 // Enabling Dev Mode
 // To Enable DevMode Uncomment The Line Below
 // Symphony\Core::enableDevMode();
+
+// Setting JSON As Default Configurations File
+// Symphony\Configurations::useJSON();
 
 // Starting Symphony
 Symphony\Core::start();
