61ce7dde937b1037142bc296b0116b58e62a727e
woXrooX <woXrooX@gmail.com>
2022-11-13 15:52:34 +0100

    + Example in index.php

--- source/index.php
+++ source/index.php
@@ -1,6 +1,9 @@
 <?php
 require_once 'php/core.php';
 
+// Setting JSON As Default Configurations File
+// Symphony\Configurations::useJSON();
+
 // Enabling Dev Mode
 // To Enable DevMode Uncomment The Line Below
 // Symphony\Core::enableDevMode();
